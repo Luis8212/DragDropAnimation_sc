e2a4aff79dace5a9e77461e6882f83982bc54016
luis8212 <luisclopez1999@gmail.com>
2022-11-27 15:57:08 -0500

    drap

--- app/src/main/java/edu/farmingdale/alrajab/dragdropanimation_sc/DragAndDropViews.kt
+++ app/src/main/java/edu/farmingdale/alrajab/dragdropanimation_sc/DragAndDropViews.kt
@@ -22,7 +22,7 @@ class DragAndDropViews : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDragAndDropViewsBinding.inflate(layoutInflater)
 
-        setContentView(binding.root)
+         setContentView(binding.root)
         binding.holder01.setOnDragListener(arrowDragListener)
         binding.holder02.setOnDragListener(arrowDragListener)
         binding.holder03.setOnDragListener(arrowDragListener)
